196e5abfd40852b56188575b01f88d4d4609e1e6
vinay-lekkalapudi <vinaymtvs@gmail.com>
2022-05-25 13:44:00 +0530

    your msg

---
+++ users/src/components/pages/Home.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  const [users, setUser] = useState([]);
+
+  useEffect(() => {
+    loadUsers();
+    // eslint-disable-next-line
+  }, []);
+
+  const loadUsers = async () => {
+    const result = await axios.get("http://localhost:5000/users");
+    setUser(result.data.reverse());
+    
+  };
+
+  const deleteUser = async id => {
+    await axios.delete(`http://localhost:5000/users/${id}`);
+    loadUsers();
+  };
+
+  return (
+    <div className="container">
+      <div className="py-4">
+        <h1>Home Page</h1>
+        <table className="table border shadow">
+          <thead className="thead-dark">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">User Name</th>
+              <th scope="col">Email</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr>
+                <th scope="row">{index + 1}</th>
+                <td>{user.name}</td>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+                <td>
+                  <Link className="btn btn-primary mr-2" to={`/users/${user.id}`}>
+                    View
+                  </Link>
+                  <Link
+                    className="btn btn-outline-primary mr-2"
+                    to={`/users/edit/${user.id}`}
+                  >
+                    Edit
+                  </Link>
+                  <Link
+                    className="btn btn-danger"
+                    onClick={() => deleteUser(user.id)}
+                  >
+                    Delete
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
